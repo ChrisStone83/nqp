7b5a6f912795eb96857374469dc9bb3fc6b56bcd
Your Name <you@example.com>
2025-04-06 14:03:51 +0200

    add calculate()

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 "use strict";
-function draw(x1,x2){
 
-}
 function mitternacht(a,b,c){
     var x = []
     x[0] = (-b + Math.sqrt((b*b)-4*a*c))/(2*a)
     x[1] = (-b - Math.sqrt((b*b)-4*a*c))/(2*a)
     return x
 }
+function calculate(){
+    
+var a = parseFloat(document.getElementById("a").value)
+var b = parseFloat(document.getElementById("b").value)
+var c = parseFloat(document.getElementById("c").value)
 
-document.getElementById("test").innerHTML= mitternacht(1,1,1)
+var p = document.createElement("p")
+p.innerHTML = mitternacht(a,b,c)
+var ergebnisDiv = document.getElementById("ergebis")
+ergebnisDiv.appendChild(p)
+
+}
+
+function drawOnCanvas(){
+    var canvas = document.getElementsByTagName("canvas");
+    var c = canvas.getContext("2d");
+
+}
