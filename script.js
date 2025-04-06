c154114cd598cedda32e950f5699e027e43c070c
Your Name <you@example.com>
2025-04-06 13:04:13 +0200

    Mitternachtsformel hinzugefügt

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+function draw(x1,x2){
+
+}
+function mitternacht(a,b,c){
+    var x = []
+    x[0] = (-b + Math.sqrt((b*b)-4*a*c))/(2*a)
+    x[1] = (-b - Math.sqrt((b*b)-4*a*c))/(2*a)
+    return x
+}
+
+document.getElementById("test").innerHTML= mitternacht(1,1,1)
