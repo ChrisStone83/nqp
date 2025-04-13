a9e88fc2d9f8a219487c072487ef8b01fd32134a
Your Name <you@example.com>
2025-04-13 11:44:28 +0200

    Problem mit verschwindetem P Element gelöst und richtige Ergebnisse.

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-"use strict";
+'use strict'
 
 function mitternacht(a,b,c){
     var x = []
-    x[0] = (-b + Math.sqrt((b*b)-4*a*c))/(2*a)
-    x[1] = (-b - Math.sqrt((b*b)-4*a*c))/(2*a)
-    return x
+    x[0] = (-b + Math.sqrt((b*b)-4*a*c))/(2*a);
+    x[1] = (-b - Math.sqrt((b*b)-4*a*c))/(2*a);
+    return x;
 }
 function calculate(){
-    
-var a = parseFloat(document.getElementById("a").value)
-var b = parseFloat(document.getElementById("b").value)
-var c = parseFloat(document.getElementById("c").value)
+   
+var a = parseFloat(document.getElementById("a").value);
+var b = parseFloat(document.getElementById("b").value);
+var c = parseFloat(document.getElementById("c").value);
+var x = mitternacht(a,b,c);
+var ergText = "Das Polynom " + a +"x^2 +" + b + "x +" + c +" hat die Nullstellen x1 = "+ x[0] + ", x2= "+ x[1]; 
 
-var p = document.createElement("p")
-p.innerHTML = mitternacht(a,b,c)
-var ergebnisDiv = document.getElementById("ergebis")
-ergebnisDiv.appendChild(p)
+const ergP = document.createElement("p");
+ergP.innerHTML = ergText;
+document.getElementById("ergebnis").innerHTML = "";
+document.getElementById("ergebnis").appendChild(ergP);
 
+return false
 }
 
 function drawOnCanvas(){
