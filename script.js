06e039a55dd306876d49ec767770214dab3346f4
Your Name <you@example.com>
2025-04-21 16:12:58 +0200

    sad

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+
+
 function mitternacht(a,b,c){
     var x = []
     x[0] = (-b + Math.sqrt((b*b)-4*a*c))/(2*a);
@@ -8,22 +10,33 @@ function mitternacht(a,b,c){
 }
 function calculate(){
    
-var a = parseFloat(document.getElementById("a").value);
-var b = parseFloat(document.getElementById("b").value);
-var c = parseFloat(document.getElementById("c").value);
-var x = mitternacht(a,b,c);
-var ergText = "Das Polynom " + a +"x^2 +" + b + "x +" + c +" hat die Nullstellen x1 = "+ x[0] + ", x2= "+ x[1]; 
+    var a = parseFloat(document.getElementById("a").value);
+    var b = parseFloat(document.getElementById("b").value);
+    var c = parseFloat(document.getElementById("c").value);
+    var x = mitternacht(a,b,c);
+    var ergText = "Das Polynom " + a +"x^2 +" + b + "x +" + c +" hat die Nullstellen x1 = "+ x[0] + ", x2= "+ x[1]; 
 
-const ergP = document.createElement("p");
-ergP.innerHTML = ergText;
-document.getElementById("ergebnis").innerHTML = "";
-document.getElementById("ergebnis").appendChild(ergP);
+    const ergP = document.createElement("p");
+    ergP.innerHTML = ergText;
+    document.getElementById("ergebnis").innerHTML = "";
+    document.getElementById("ergebnis").appendChild(ergP);
 
-return false
-}
-
-function drawOnCanvas(){
-    var canvas = document.getElementsByTagName("canvas");
-    var c = canvas.getContext("2d");
 
 }
+
+function drawCoordInCanvas(){
+    console.log("drawCoordInCanvas aufgerufen");
+    var canvas = document.getElementById("CV");
+    if (!canvas) {
+        console.error("Canvas-Element nicht gefunden!");
+        return;
+    }
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+}
+window.onload = function() {
+    drawCoordInCanvas();
+};
